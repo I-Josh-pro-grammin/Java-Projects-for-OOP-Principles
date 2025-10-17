fb4cae98a34ce29352f0c991df9a6d5b8eb6181a
IZERE JOSHUA <izerejoshua94@gmail.com>
2025-10-17 15:04:16 +0200

    Tokenisation using scanner

--- FileHandler/IOStream.java
+++ FileHandler/IOStream.java
@@ -4,15 +4,45 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
-public class IOStream {
-    public static void main(String[]  args) throws IOException {
-        File file = new File("D:\\LECTURES\\MY CODES\\Java\\New\\Teacher's exercises\\FileHandler\\myFile");
-      try(PrintWriter writer = new PrintWriter(new FileWriter(file, true))) {
-          writer.println("Hello World");
-          writer.println("Still in java...");
-        }catch(IOException e) {
-          System.out.println(e.getMessage());
-        }
+//public class IOStream {
+//    public static void main(String[]  args) throws IOException {
+//        File file = new File("D:\\LECTURES\\MY CODES\\Java\\New\\Teacher's exercises\\FileHandler\\myFile");
+////      try(PrintWriter writer = new PrintWriter(new FileWriter(file, true))) {
+////          writer.println("Hello World");
+////          writer.println("Still in java...");
+////        }catch(IOException e) {
+////          System.out.println(e.getMessage());
+////        }
+//        Scanner input = new Scanner(System.in);
+//        System.out.println(("enter the data, type 'exit' to exit"));
+//        String line = input.nextLine();
+//        while (input.hasNext()) {
+//            line = input.nextLine();
+//            if(line.equalsIgnoreCase("exit")) {
+//                break;
+//            }
+//            System.out.println("Entered: " + line);
+//        }
+//        input.close();
+//    }
+//
+//}
+
+class Delimeter {
+    public static void main(String [] args) {
+//        String data = "Honorine turamuha bombo 5";
+        String data2 = "17/10/2025";
+
+        Scanner input = new Scanner(data2);
+        input.useDelimiter("/");
+        int token1 = input.nextInt();
+        int token2 = input.nextInt();
+        int token3 = input.nextInt();
+
+        int sum = token1 + token2 + token3;
+
+        System.out.println("sum is: " + sum);
     }
 }
