55b355e63a820d749d999a0d5705512941269a5b
IZERE JOSHUA <izerejoshua94@gmail.com>
2025-11-27 11:19:50 +0200

    threads in java

---
+++ Consumer&&producer/ProducerConsumerMain.java
@@ -0,0 +1,28 @@
+public class ProducerConsumerMain {
+  public static void main (String[] args) {
+    ProducerConsumer pc = new ProducerConsumer(10);
+
+    Thread t1 = new Thread(() -> {
+      for(int i=0; i<=50; i++) {
+        try {
+          pc.produce(i);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  
+    Thread t2 = new Thread(() -> {
+      for(int i=0; i<=50; i++) {
+        try {
+          pc.consume();
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  
+    t1.start();
+    t2.start();
+  }
+}
